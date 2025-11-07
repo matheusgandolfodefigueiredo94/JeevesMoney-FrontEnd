49955fb49bf7db64422e261c3fb24614bf8582ef
Matheus Figueiredo <matheusgandolfodefigueiredo@gmail.com>
2025-11-06 22:58:26 -0300

    alteração environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
   // Adicione as linhas abaixo
+  apiUrl: 'https://jeevesmoney-backend.onrender.com',
   supabaseUrl: 'https://vcdznpxbnbzelsgorbmq.supabase.co',
   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZjZHpucHhibmJ6ZWxzZ29yYm1xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE3NzU1OTgsImV4cCI6MjA3NzM1MTU5OH0.zSbbFPtX4ej7sH6eM-gXiTk91S6VfSJdrtMTET-wc54' 
 };
