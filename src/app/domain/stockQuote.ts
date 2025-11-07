9ad8babcdf5247e384dc8bc02a2d92d3dfa6b379
Matheus Figueiredo <matheusgandolfodefigueiredo@gmail.com>
2025-11-06 23:39:21 -0300

    Alteração portifolio

--- src/app/domain/stockQuote.ts
+++ src/app/domain/stockQuote.ts
@@ -1,6 +1,12 @@
 export interface StockQuote {
   symbol: string;
   price: number;
+  change: number;
+  changePercent: number;
+  previousClose: number;
+  open: number;
+  high: number;
+  low: number;
   currency?: string;
   timestamp?: string;
 }
--- src/app/pages/portfolio/portfolio-view/portfolio-view.ts
+++ src/app/pages/portfolio/portfolio-view/portfolio-view.ts
@@ -1,42 +1,47 @@
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
-import { CommonModule,DecimalPipe } from '@angular/common';
+import { ChangeDetectorRef,Component, OnInit } from '@angular/core';
+import { CommonModule, DecimalPipe } from '@angular/common';
+import { forkJoin } from 'rxjs';
 import { StockService } from '../../../services/stock.service';
 import { StockQuote } from '../../../domain/stockQuote';
 
-
 @Component({
   selector: 'app-portfolio-view',
- standalone: true,
-  imports: [CommonModule,DecimalPipe],
+  standalone: true,
+  imports: [CommonModule, DecimalPipe],
   templateUrl: './portfolio-view.html',
   styleUrls: ['./portfolio-view.scss'],
 })
 export class PortfolioView implements OnInit {
-  stockSymbol = 'PETR4';
-  stockPrice: number | null = null;
+  stocks: StockQuote[] = [];
   isLoading = false;
   errorMessage = '';
+  private stockSymbols = ['PETR4', 'VALE3', 'ITUB4', 'BBDC4', 'MGLU3'];
 
   constructor(private stockService: StockService,private cdr: ChangeDetectorRef) {}
 
   ngOnInit() {
-    this.fetchStockQuote();
+    this.loadPortfolio();
   }
 
-  fetchStockQuote() {
+  loadPortfolio() {
     this.isLoading = true;
     this.errorMessage = '';
-    this.stockService.getQuote(this.stockSymbol)
-      .subscribe({
-        next: (data: StockQuote) => {
-          this.stockPrice = data.price;
-          this.isLoading = false;
-          this.cdr.detectChanges();
-        },
-        error: (err) => {
-          this.errorMessage = 'Erro ao buscar cotação.';
-          this.isLoading = false;
-        }
-      });
+    this.stocks = [];
+
+    const observables = this.stockSymbols.map(symbol => 
+      this.stockService.getQuote(symbol)
+    );
+
+    forkJoin(observables).subscribe({
+      next: (quotes) => {
+        this.stocks = quotes;
+        this.isLoading = false;
+        this.cdr.detectChanges();
+      },
+      error: (err) => {
+        this.errorMessage = 'Erro ao buscar cotações. Por favor, tente novamente mais tarde.';
+        this.isLoading = false;
+      }
+    });
   }
 }
